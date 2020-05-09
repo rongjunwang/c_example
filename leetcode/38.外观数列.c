2ae8c7fb3b43af80852246d1d0d00be6862a5b88
王荣军 <wangrongjun@chelun.com>
2020-05-09 18:28:35 +0800

    add

--- leetcode/38.外观数列.c
+++ leetcode/38.外观数列.c
@@ -8,7 +8,33 @@
 
 
 char * countAndSay(int n){
+    char** dp = (char**)malloc(sizeof(char*) * n);
+    for (int i = 1; i < n; i++)
+    {
+        dp[i] = (char*)malloc(sizeof(char)*100); 
+    }
 
+    dp[0] = "1";
+
+    int count = 0;
+    int i,j;
+    for (i = 1; i < n; i++)
+    {
+        for (j = 1; j< strlen(dp[i-1]); j++)
+        {
+            if(dp[i-1][j] == dp[i-1][j-1]){
+                count++;
+            }else{
+                dp[i][j] = count + dp[i-1][j];
+            }
+            printf("%d-%c\n",count,dp[i][j]);
+        }
+        printf("%d\n",strlen(dp[i-1]));
+        dp[i][j+1] = '\0';
+    }
+    
+    
+    return dp[n-1];
 }
 
 
--- leetcode/72.编辑距离.c
+++ leetcode/72.编辑距离.c
@@ -13,34 +13,33 @@ int minDistance(char * word1, char * word2){
     if(word1_len == 0) return word2_len;
     if(word2_len == 0) return word1_len;
 
-    int dp[word1_len][word2_len];
+    int dp[word1_len+1][word2_len+1];
     memset(dp, 0, sizeof(dp));
 
-    dp[0][0] = 1;
-    for (int i = 1; i < word1_len; i++)
+    dp[0][0] = 0;
+    for (int i = 1; i <= word1_len; i++)
     {
         dp[i][0] =  dp[i-1][0] + 1;
     }
 
-    for (int j = 1; j < word2_len; j++)
+    for (int j = 1; j <= word2_len; j++)
     {
         dp[0][j] = dp[0][j-1] + 1;
     }
-    
-    for (int i = 1; i < word1_len; i++)
+
+    for (int i = 1; i <= word1_len; i++)
     {
-        for (int j = 1; j < word2_len; j++)
+        for (int j = 1; j <= word2_len; j++)
         {
-            if(*(word1+i) == *(word2+j)){
+            if(word1[i-1] == word2[j-1]){
                 dp[i][j] = dp[i-1][j-1];
             }else{
                 dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) + 1; 
-                printf("%d-%d-%d-%d-%d-%d\n",i,j,dp[i-1][j],dp[i][j-1],dp[i-1][j-1],dp[i][j]);
             }
         }
         
     }
-    return dp[word1_len-1][word2_len-1];
+    return dp[word1_len][word2_len];
 
 }
 
